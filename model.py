5816c34bce23ec9fbb9d6c5ecfcecce4cf9431d5
notWorthy <developer@mailible.com>
2020-08-31 23:58:43 +0000

    Added an allocate domain service and made Batch sortable by eta

--- model.py
+++ model.py
@@ -2,6 +2,10 @@ from dataclasses import dataclass
 from datetime import date
 from typing import Optional, Set
 
+class OutOfStock(Exception):
+    pass
+
+
 @dataclass(frozen=True)
 class OrderLine:
     orderid: str
@@ -22,6 +26,13 @@ class Batch:
         if not isinstance(other, Batch):
             return False
         return other.reference == self.reference
+
+    def __lt__(self, other):
+        if not self.eta:
+            return True
+        if not other.eta:
+            return False
+        return self.eta < other.eta
 
     def __hash__(self):
         return hash(self.reference)
@@ -44,3 +55,12 @@ class Batch:
     def deallocate(self, line:OrderLine):
         if line in self._allocations:
             self._allocations.remove(line)
+
+def allocate(line: OrderLine, batches: [Batch]) -> str:
+    try:
+        batch = next(batch for batch in sorted(batches) if batch.can_allocate(line))
+    except StopIteration:
+        raise(OutOfStock(f"Out of stock for sku: {line.sku}"))
+    batch.allocate(line)
+    return batch.reference
+
---
+++ test_allocate.py
@@ -0,0 +1,44 @@
+from datetime import date, timedelta
+
+import pytest
+
+from model import allocate, Batch, OrderLine, OutOfStock
+
+today = date.today()
+tomorrow = today + timedelta(days=1)
+later = today + timedelta(weeks=2)
+
+def test_prefers_current_stock_batches_to_shipments():
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    line = OrderLine("oref", "RETRO-CLOCK", 10)
+
+    allocate(line, [in_stock_batch, shipment_batch])
+    assert in_stock_batch.available_quantity == 90
+    assert shipment_batch.available_quantity == 100
+
+def test_prefers_earlier_batches():
+    earliest = Batch("speedy-batch", "RETRO-CLOCK", 100, eta=today)
+    medium = Batch("normal-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    latest = Batch("slow-batch", "RETRO-CLOCK", 100, eta=later)
+    line = OrderLine("oref", "RETRO-CLOCK", 10)
+    allocate(line, [medium, latest, earliest])
+    assert earliest.available_quantity == 90
+    assert medium.available_quantity == 100
+    assert latest.available_quantity == 100 
+
+def test_returns_allocated_batch_ref():
+    in_stock_batch = Batch("in-stock-batch", "RETRO-CLOCK", 100, eta=None)
+    shipment_batch = Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow)
+    line = OrderLine("oref", "RETRO-CLOCK", 10)
+
+    result = allocate(line, [in_stock_batch, shipment_batch])
+    assert result == in_stock_batch.reference
+
+def test_raises_out_of_stock_exception_if_cannot_allocate():
+    batch = Batch('batch1', 'SMALL-FORK', 10, eta=today)
+    allocate(OrderLine('order1', 'SMALL-FORK', 10), [batch])
+    with pytest.raises(OutOfStock, match='SMALL-FORK'):
+        allocate(OrderLine('order2', 'SMALL-FORK', 1), [batch])
+
+
