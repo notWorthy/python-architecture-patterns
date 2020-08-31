3128df44b55275f941dc9b8ca2531a8fb2af8477
notWorthy <developer@mailible.com>
2020-08-31 21:48:07 +0000

    add return type hint for Batch.can_allocate

--- model.py
+++ model.py
@@ -20,5 +20,5 @@ class Batch:
     def allocate(self, line: OrderLine):
         self.available_quantity -= line.qty
 
-    def can_allocate(self, line: OrderLine):
+    def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
