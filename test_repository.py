f1034b687c80b07b5e9ab811dc576e1d28e3b14c
notWorthy <developer@mailible.com>
2020-09-03 01:23:46 +0000

    initial orm repository stuff

---
+++ test_repository.py
@@ -0,0 +1,14 @@
+import model
+import repository
+
+def test_repository_can_save_a_batch(session):
+    batch = model.Batch("batch1", "RUSTY-SOAPDISH", 100, eta=None)
+
+    repo = repository.SqlAlchemyRepository(session)
+    repo.add(batch)
+    session.commit()
+
+    rows = list(session.execute(
+        'SELECT reference, sku, _purchased_quantity, eta FROM "batches"'
+    ))
+    assert rows == [("batch1", "RUSTY-SOAPDISH", 100, None)]
